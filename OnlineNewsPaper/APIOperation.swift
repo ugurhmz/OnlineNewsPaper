dd8f71944b330a70dd18969cdf02f203c91f89f5
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-02-09 22:38:14 +0300

    SearchQuery with url param added

--- OnlineNewsPaper/APIOperation.swift
+++ OnlineNewsPaper/APIOperation.swift
@@ -9,10 +9,13 @@ final class APIOperation {
     static let company = "apple"
     static let pageSize = "70"
 
+    
     static let myUrl = "https://newsapi.org/v2/everything?q=\(company)&from=2022-02-08&to=2022-02-08&sortBy=popularity&pageSize=\(pageSize)&apiKey=be74c7f94ab24622b6cb92cfe66c6542"
     
     struct ConstantInfos {
         static let topHeadlinesURL = URL(string: myUrl)
+        
+        static let searchByQuery = "https://newsapi.org/v2/everything?sortedBy=popularity&pageSize=\(pageSize)&apiKey=be74c7f94ab24622b6cb92cfe66c6542&q="
     }
     
     
@@ -20,6 +23,7 @@ final class APIOperation {
     
     
     
+    // API  REGULAR
     public  func getTopNews(completion: @escaping (Result<[Article], Error>) -> Void) {
         
         
@@ -48,6 +52,47 @@ final class APIOperation {
             }
         }
         task.resume()
+    }
+    
+    
+    
+    // API BY QUERY
+    public func search(with query : String, completion: @escaping (Result<[Article], Error>) -> Void) {
+        
+        guard !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        let urlString = ConstantInfos.searchByQuery + query
+        
+        
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                
+                do {
+                    
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    print("Search arr size : ", result.articles.count)
+                    completion(.success(result.articles))
+                    
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        task.resume()
+        
     }
     
     
